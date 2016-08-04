9d9eaa416478e2e17c47108f734c9eada62565a3
Christopher De Cairos <cade@mozillafoundation.org>
2016-08-04 16:06:01 -0400

    add the WIP moz config

---
+++ public/config.js
@@ -0,0 +1,22 @@
+
+var config = {
+
+   repo: 'MozillaFoundation/Advocacy',
+
+   weekDaysOff: [0,6],
+
+   colorByDev: {
+      "cadecairos": "ganttBlue",
+      "ScottDowne": "ganttRed"
+   },
+
+   holidays: {
+   },
+
+
+   excludedMilestones: [
+   ],
+
+   defaultDuration: 1 // in days
+
+};
